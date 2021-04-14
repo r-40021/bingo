99c40f3164c6dc01cb836c433a9ede4622bc3e90
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-04-14 21:11:26 +0900

    レイアウト完成2

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,33 @@
+var history,max,select;
 document.addEventListener("DOMContentLoaded", function () {
   resize();
+  if (localStorage.getItem('history')) {
+    
+  }
+  
 });
-document.addEventListener("resize", function () {
+window.addEventListener("resize", function () {
   resize();
+});
+document.addEventListener("DOMContentLoaded", function () {
+  var range = document.getElementById("bingoMax");
+  var rangeText = document.getElementById("bingoMaxText");
+  range.addEventListener("input", function () {
+    rangeText.value = range.value;
+  });
+  rangeText.addEventListener("input", function () {
+    range.value = rangeText.value;
+  });
 });
 function resize() {
   var numberElement = document.getElementById("bingoNumber");
-  var bodyElement = document.getElementById("body");
-  if (numberElement.offsetHeight > bodyElement.offsetHeight) {
-    numberElement.style.height = "90%";  
-    numberElement.style.width = numberElement.offsetHeight + "px";
-  } else {
+  var wrapElement = document.getElementById("number-wrap");
+  if (wrapElement.clientHeight > wrapElement.clientWidth) {
+    numberElement.style.width = "90%";
     numberElement.style.height = numberElement.offsetWidth + "px";
+  } else {
+    numberElement.style.height = "90%";
+    numberElement.style.width = numberElement.offsetHeight + "px";
   }
   numberElement.style.fontSize = (numberElement.offsetHeight / 3) * 2 + "px";
 }
