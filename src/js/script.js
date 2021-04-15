4711ac650868f1e504fdb0f498b962c08ef16f0f
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-04-15 17:59:09 +0900

    BINGO基本機能完成

--- src/js/script.js
+++ src/js/script.js
@@ -1,53 +1,83 @@
-var history, max, select;
-var colorList = ["#ffa500","#d3e15c","#b384c7","#F06060","#a9ceec"];
+var max = 75;
+var myHistory = [];
+var select = [];
+var colorList = ["#ffa500", "#d3e15c", "#b384c7", "#F06060", "#a9ceec"];
 document.addEventListener("DOMContentLoaded", function () {
   resize();
-  if (localStorage.getItem("history")) {
-    history = localStorage.getItem("history");
+  if (localStorage.getItem("myHistory")) {
+    myHistory = localStorage.getItem("myHistory");
   }
   if (localStorage.getItem("max")) {
-    max = localStorage.getItem("max");
+    max = Number(localStorage.getItem("max"));
     document.getElementById("bingoMax").value = max;
     document.getElementById("bingoMaxText").value = max;
   }
+  addSelect();
 });
 window.addEventListener("resize", function () {
   resize();
 });
 document.addEventListener("DOMContentLoaded", function () {
-  max = 75;
   var range = document.getElementById("bingoMax");
   var rangeText = document.getElementById("bingoMaxText");
   range.addEventListener("input", function () {
     rangeText.value = range.value;
     max = range.value;
+    addSelect();
   });
   rangeText.addEventListener("input", function () {
     range.value = rangeText.value;
     max = range.value;
+    addSelect();
+    if (rangeText.value < 1) {
+      rangeText.value = 1;
+    }
+    if (rangeText.value > 99) {
+      rangeText.value = 99;
+    }
   });
 });
 function spin() {
-  var count = 0;
-  var numberElement = document.getElementById("bingoNumber");
-  var stop = Math.floor(Math.random() * (20 - 5) + 5);
-  var historyBody = document.getElementById("history-body");
-  var spin = setInterval(() => {
-    var min = Math.ceil(1);
-    max = Math.floor(max);
-    var display = Math.floor(Math.random() * (max - min) + min);
-    document.getElementById("number-inner").innerHTML = display;
-    numberElement.style.borderColor = colorList[count % colorList.length];
-    count++;
-    if (count >= stop) {
-      clearInterval(spin);
-      var div = document.createElement("div");
-      div.className = "history-number";
-      div.innerHTML = display;
-      historyBody.appendChild(div);
-      historyBody.scroll(0, historyBody.scrollHeight - historyBody.clientHeight);
-    }
-  }, 100);
+  if (select.length <= 0) {
+    var endModal = new bootstrap.Modal(document.getElementById("end-modal"));
+    endModal.show();
+  } else {
+    makeDisable("spin");
+    makeDisable("reset");
+    makeDisable("bingoMax");
+    makeDisable("bingoMaxText");
+    makeDisable("formFileSm");
+    var count = 0;
+    var numberElement = document.getElementById("bingoNumber");
+    var stop = Math.floor(Math.random() * (15 - 5) + 5);
+    var historyBody = document.getElementById("history-body");
+    var spin = setInterval(() => {
+      var index = Math.floor(Math.random() * select.length);
+      var display = select[index];
+      document.getElementById("number-inner").innerHTML = display;
+      numberElement.style.borderColor = colorList[count % colorList.length];
+      count++;
+      if (count >= stop) {
+        clearInterval(spin);
+        var div = document.createElement("div");
+        div.className = "history-number";
+        div.innerHTML = display;
+        myHistory.push(display);
+        select.splice(select.indexOf(display), 1);
+        historyBody.appendChild(div);
+        historyBody.scroll(
+          0,
+          historyBody.scrollHeight - historyBody.clientHeight
+        );
+        removeDisable("spin");
+        removeDisable("reset");
+        removeDisable("bingoMax");
+        removeDisable("bingoMaxText");
+        removeDisable("formFileSm");
+        document.getElementById("spin").focus();
+      }
+    }, 100);
+  }
 }
 
 function resize() {
@@ -61,4 +91,19 @@ function resize() {
     numberElement.style.width = numberElement.offsetHeight + "px";
   }
   numberElement.style.fontSize = (numberElement.offsetHeight / 3) * 2 + "px";
+}
+function addSelect() {
+  select = [];
+  localStorage.setItem("max", max);
+  for (let i = 0; i < max; i++) {
+    if (myHistory.indexOf(i + 1) === -1) {
+      select.push(i + 1);
+    }
+  }
+}
+function makeDisable(element) {
+  document.getElementById(element).setAttribute("disabled", null);
+}
+function removeDisable(element) {
+  document.getElementById(element).removeAttribute("disabled");
 }
