f7116ffc7c0c101a9805c9a0d634cc7459094511
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-04-15 21:29:31 +0900

    アプリとして形にした

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,18 @@ var colorList = ["#ffa500", "#d3e15c", "#b384c7", "#F06060", "#a9ceec"];
 document.addEventListener("DOMContentLoaded", function () {
   resize();
   if (localStorage.getItem("myHistory")) {
-    myHistory = localStorage.getItem("myHistory");
+    var his = localStorage.getItem("myHistory");
+    myHistory = JSON.parse(his);
+    var historyBody = document.getElementById("history-body");
+    for (let i = 0; i < myHistory.length; i++) {
+      var div = document.createElement("div");
+      div.className = "history-number";
+      div.innerHTML = myHistory[i];
+      historyBody.appendChild(div);
+    }
+    document.getElementById("number-inner").innerHTML =
+      myHistory[myHistory.length - 1];
+    historyBody.scroll(0, historyBody.scrollHeight - historyBody.clientHeight);
   }
   if (localStorage.getItem("max")) {
     max = Number(localStorage.getItem("max"));
@@ -13,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("bingoMaxText").value = max;
   }
   addSelect();
+  removeDisableSet();
 });
 window.addEventListener("resize", function () {
   resize();
@@ -42,11 +54,7 @@ function spin() {
     var endModal = new bootstrap.Modal(document.getElementById("end-modal"));
     endModal.show();
   } else {
-    makeDisable("spin");
-    makeDisable("reset");
-    makeDisable("bingoMax");
-    makeDisable("bingoMaxText");
-    makeDisable("formFileSm");
+    makeDisableSet();
     var count = 0;
     var numberElement = document.getElementById("bingoNumber");
     var stop = Math.floor(Math.random() * (15 - 5) + 5);
@@ -63,18 +71,14 @@ function spin() {
         div.className = "history-number";
         div.innerHTML = display;
         myHistory.push(display);
+        localStorage.setItem("myHistory", JSON.stringify(myHistory));
         select.splice(select.indexOf(display), 1);
         historyBody.appendChild(div);
         historyBody.scroll(
           0,
           historyBody.scrollHeight - historyBody.clientHeight
         );
-        removeDisable("spin");
-        removeDisable("reset");
-        removeDisable("bingoMax");
-        removeDisable("bingoMaxText");
-        removeDisable("formFileSm");
-        document.getElementById("spin").focus();
+        removeDisableSet();
       }
     }, 100);
   }
@@ -104,6 +108,74 @@ function addSelect() {
 function makeDisable(element) {
   document.getElementById(element).setAttribute("disabled", null);
 }
+function makeDisableSet() {
+  makeDisable("spin");
+  makeDisable("reset");
+  makeDisable("bingoMax");
+  makeDisable("bingoMaxText");
+}
 function removeDisable(element) {
   document.getElementById(element).removeAttribute("disabled");
+}
+function removeDisableSet() {
+  removeDisable("spin");
+  removeDisable("reset");
+  removeDisable("bingoMax");
+  removeDisable("bingoMaxText");
+  document.getElementById("spin").focus();
+}
+function resetAsk() {
+  new bootstrap.Modal(document.getElementById("reset-modal")).show();
+}
+function reset() {
+  makeDisableSet();
+  localStorage.removeItem("myHistory");
+  localStorage.removeItem("max");
+  document.getElementById("number-inner").innerHTML = null;
+  document.getElementById("history-body").innerHTML = null;
+  document.getElementById("bingoNumber").style.borderColor = colorList[0];
+  max = 75;
+  myHistory = [];
+  select = [];
+  addSelect();
+  document.getElementById("bingoMax").value = max;
+  document.getElementById("bingoMaxText").value = max;
+  removeDisableSet();
+}
+function once() {
+  makeDisableSet();
+  localStorage.removeItem("myHistory");
+  document.getElementById("number-inner").innerHTML = null;
+  document.getElementById("history-body").innerHTML = null;
+  document.getElementById("bingoNumber").style.borderColor = colorList[0];
+  myHistory = [];
+  select = [];
+  addSelect();
+  removeDisableSet();
+}
+function storageAvailable(type) {
+  var storage;
+  try {
+    storage = window[type];
+    var x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
 }
