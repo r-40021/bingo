86e44459ec701a7138c947a76f6485a90cd328a1
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-04-16 17:11:48 +0900

    Change theme color and bg-color

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '20210416';
+const CACHE_NAME = '20210416v2';
 const urlsToCache = [
   'index.html',
   './src/css/style.css',
@@ -21,7 +21,7 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('activate', function (event) {
 
-  var cacheAllowlist = ['20210416'];
+  var cacheAllowlist = ['20210416v2'];
 
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
