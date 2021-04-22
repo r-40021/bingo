54a66c7f317960bd7afc94fdf0e623b727f35a55
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-04-22 19:51:55 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /*参考:https://developers.google.com/web/fundamentals/primers/service-workers*/
-var CACHE_NAME = "bi-20210422v2";
+var CACHE_NAME = "bi-20210422v3";
 var urlsToCache = [
   "index.html",
   "./src/css/style.css",
