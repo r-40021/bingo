d02404123302d6aaa24eb4391937ad7ca302c117
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-04-23 17:13:05 +0900

    デプロイ

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /*参考:https://developers.google.com/web/fundamentals/primers/service-workers*/
-var CACHE_NAME = "bi-20210422v3";
+var CACHE_NAME = "bi-20210423";
 var urlsToCache = [
   "index.html",
   "./src/css/style.css",
