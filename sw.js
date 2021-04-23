329a64c756994914a4cce750871487fdde24d08b
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-04-23 22:50:57 +0900

    デザイン大幅変更

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /*参考:https://developers.google.com/web/fundamentals/primers/service-workers*/
-var CACHE_NAME = "bi-20210423v2";
+var CACHE_NAME = "bi-20210423v3";
 var urlsToCache = [
   "index.html",
   "./src/css/style.css",
