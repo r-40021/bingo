e482334de5b7aec59f9fe0024a4fc69a882f9371
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-04-24 17:33:43 +0900

    absoluteなくす

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /*参考:https://developers.google.com/web/fundamentals/primers/service-workers*/
-var CACHE_NAME = "bi-20210424v11";
+var CACHE_NAME = "bi-20210424v12";
 var urlsToCache = [
   "index.html",
   "./src/css/style.css",
