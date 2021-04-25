76207dc667979339b9c8409ca9e5476be8f04f37
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-04-25 09:44:43 +0900

    ローディングスタイル変更・コメント追加

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /*参考:https://developers.google.com/web/fundamentals/primers/service-workers*/
-var CACHE_NAME = "bi-20210424v15";
+var CACHE_NAME = "bi-20210425";
 var urlsToCache = [
   "index.html",
   "./src/css/style.css",
