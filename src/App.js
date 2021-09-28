8aadf3892fae9b55837acf0489c7432ebb7e3c91
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-09-28 18:06:30 +0900

    Basic Design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider, Button } from "@chakra-ui/react"
+import { MdLoop } from "react-icons/md";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="flex">
+        <div className="body">body</div>
+        <div className="footer">
+        <Button leftIcon={<MdLoop />} colorScheme="blue">Button</Button>
+        </div>
+      </div>
+    </ChakraProvider>
   );
 }
+
 
 export default App;
