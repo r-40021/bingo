e655d87cd25373f5713129c707b7e44e184d43e8
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-10-03 13:27:33 +0900

    [Add]Delete chache

---
+++ public/sw.js
@@ -0,0 +1,7 @@
+caches.keys().then(function (keyList) {
+    return Promise.all(keyList.map(function (key) {
+        if ("bi-".indexOf(key) === -1) {
+            return caches.delete(key);
+        }
+    }));
+})
