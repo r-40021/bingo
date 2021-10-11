83b37fae9a4362dfb0221c3e1f1716c3794fc398
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-10-11 17:38:24 +0900

    [Update]keyboard shortcut

--- src/undo.js
+++ src/undo.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 import { ButtonGroup, IconButton, Tooltip } from "@chakra-ui/react";
 import { MdUndo, MdRedo } from "react-icons/md";
+import { useKey } from 'react-use';
 
 export function Undo(props) {
+    const ua = window.navigator.userAgent.toLowerCase();
+    const ctrlKey = /iphone|ipad|mac os x/.test(ua) ? "⌘" : "Ctrl";
     const undo = () => {
         const newIndex = props.props.nowIndex - 1;
         props.props.changeIndex(newIndex);
@@ -28,6 +31,13 @@ export function Undo(props) {
         }
     }
 
+    const handleKeydownUndo = (e) => e.ctrlKey && e.key === 'z' && !props.props.isSpin && props.props.nowIndex >= 0 && props.props.bingoHistory.length > 0;
+
+    const handleKeydownRedo = (e) => e.ctrlKey && e.key === 'y' && !props.props.isSpin && props.props.nowIndex < props.props.bingoHistory.length - 1 && props.props.bingoHistory.length > 0;
+
+    useKey(handleKeydownUndo, undo, {event: 'keyup'});
+    useKey(handleKeydownRedo, redo, {event: 'keyup'});
+
     return (
         <ButtonGroup
             size="md"
@@ -35,10 +45,10 @@ export function Undo(props) {
             variant="outline"
             className="undoGroup"
         >
-            <Tooltip label="戻る">
+            <Tooltip label={`戻る (${ctrlKey}+Z)`}>
                 <IconButton aria-label="戻る" icon={<MdUndo />} disabled={props.props.isSpin || props.props.nowIndex < 0 || props.props.bingoHistory.length === 0} onClick={undo} />
             </Tooltip>
-            <Tooltip label="進む">
+            <Tooltip label={`進む (${ctrlKey}+Y)`}>
                 <IconButton aria-label="進む" icon={<MdRedo />} disabled={props.props.isSpin || props.props.nowIndex >= props.props.bingoHistory.length - 1 || props.props.bingoHistory.length === 0} onClick={redo} />
             </Tooltip>
         </ButtonGroup>
