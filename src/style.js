3661b497292fc67d9ef26f86265713434718f34e
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-10-13 17:15:53 +0900

    [Update]font-family

--- src/style.js
+++ src/style.js
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react"
 
 const theme = extendTheme({
   fonts: {
-    heading: `-apple-system, blinkMacSystemFont,Helvetica Neue,Segoe UI,Roboto,Arial,Hiragino Kaku Gothic ProN, "游ゴシック体", YuGothic, "游ゴシック Medium", "Yu Gothic Medium", "游ゴシック", "Yu Gothic",Meiryo,sans-serif`,
+    heading: `-apple-system, blinkMacSystemFont,Helvetica Neue,Segoe UI,Roboto,Arial,Hiragino Kaku Gothic ProN, "游ゴシック体", YuGothic, "游ゴシック", "Yu Gothic",Meiryo,sans-serif`,
     body: `-apple-system, blinkMacSystemFont,Helvetica Neue,Segoe UI,Roboto,Arial,Hiragino Kaku Gothic ProN, "游ゴシック体", YuGothic, "游ゴシック Medium", "Yu Gothic Medium", "游ゴシック", "Yu Gothic",Meiryo,sans-serif`,
   },
 })
