cdad0d636ce96ad7b01ff18d67502807cb3645c9
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-10-13 20:38:54 +0900

    [Update]UI

--- src/style.js
+++ src/style.js
@@ -5,6 +5,13 @@ const theme = extendTheme({
     heading: `-apple-system, blinkMacSystemFont,Helvetica Neue,Segoe UI,Roboto,Arial,Hiragino Kaku Gothic ProN,YuGothicM,YuGothic,Meiryo,sans-serif`,
     body: `-apple-system, blinkMacSystemFont,Helvetica Neue,Segoe UI,Roboto,Arial,Hiragino Kaku Gothic ProN,YuGothicM,YuGothic,Meiryo,sans-serif`,
   },
+  styles: {
+    global: {
+      body: {
+        lineHeight: "1.7em"
+      }
+    }
+  }
 })
 
 export default theme
