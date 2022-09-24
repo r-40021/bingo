7be55f6ff537bf788b0996399c025effb1ed9993
Frog <frog@frogapp.net>
2022-09-24 09:40:11 +0900

    Add: PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-const nextConfig = {
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  runtimeCaching,
+  sw: 'service-worker.js', 
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    scrollRestoration: true,
-  },
-}
-
-module.exports = nextConfig
+});
