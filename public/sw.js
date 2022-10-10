0f3422230a9f91710ca4c505c3911ed264b3a343
Frog <frog@frogapp.net>
2022-10-10 14:15:40 +0900

    Delete unused files

--- public/sw.js
+++
@@ -1,7 +0,0 @@
-caches.keys().then(function (keyList) {
-    return Promise.all(keyList.map(function (key) {
-        if (key.indexOf("bi-") !== -1) {
-            return caches.delete(key);
-        }
-    }));
-})
